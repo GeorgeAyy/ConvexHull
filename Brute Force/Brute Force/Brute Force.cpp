6e799db0f9044ee2665924cc1da533be1216627e
George Aziz <123029529+GeorgeAyy@users.noreply.github.com>
2023-05-29 22:37:00 +0300

    updated the algorithm

--- Brute Force/Brute Force/Brute Force.cpp
+++ Brute Force/Brute Force/Brute Force.cpp
@@ -17,45 +17,60 @@ struct Point {
 };
 
 // Function to check if three points are collinear
-bool areCollinear(const Point& p1, const Point& p2, const Point& p3) {
-    return (p2.y - p1.y) * (p3.x - p2.x) == (p3.y - p2.y) * (p2.x - p1.x);
+bool areCollinear(const Point& p1, const Point& p2, const Point& p3, int& numOperations) {
+    numOperations += 3; // Increment the operation count
+
+    double crossProduct = (p2.x - p1.x) * (p3.y - p1.y) - (p2.y - p1.y) * (p3.x - p1.x);
+    return (crossProduct == 0);
 }
 
+
+
 // Function to check if all points lie on the same side of the line
-bool arePointsOnSameSide(const Point& p1, const Point& p2, const Point& linePoint1, const Point& linePoint2) {
+bool arePointsOnSameSide(const Point& p1, const Point& p2, const Point& linePoint1, const Point& linePoint2, int& numOperations) {
+    numOperations += 5; // Increment the operation count
+
     double a = linePoint2.y - linePoint1.y;
     double b = linePoint1.x - linePoint2.x;
     double c = linePoint1.x * linePoint2.y - linePoint1.y * linePoint2.x;
 
-    bool allPointsOnSameSide = true;
-    for (const Point& p : { p1, p2 }) {
-        double result = a * p.x + b * p.y - c;
-        if (result != 0) {
-            if (allPointsOnSameSide) {
-                allPointsOnSameSide = (result > 0);
-            }
-            else {
-                return false;  // Points on different sides
-            }
+    double result1 = a * p1.x + b * p1.y - c;
+    double result2 = a * p2.x + b * p2.y - c;
+
+    if (result1 != 0 && result2 != 0) {
+        if ((result2 < 0 && result1 < 0) || (result2 > 0 && result1 > 0)) {
+            return true; // Points are on the same side
         }
+        return false; // Points are on different sides
     }
 
-    return true;  // All points are on the same side
+    return false; // Points are collinear or coincident
 }
 
+
 // Function to find the convex hull of a set of points
-std::vector<Point> findConvexHull(const std::vector<Point>& points) {
+std::vector<Point> findConvexHull(const std::vector<Point>& points, int& numOperations) {
+    numOperations = 0;
+    numOperations += 1; // Increment the operation count
+
     std::vector<Point> convexHull;
     std::set<Point> uniquePoints;  // Store unique convex hull points
 
     int n = points.size();
-    for (int i = 0; i < n; ++i) {
-        for (int j = i + 1; j < n; ++j) {
-            bool allPointsOnSameSide = true;
+    for (int i = 0; i < n; i++) { //outermost loop iterates over all the points in the vector
+        numOperations += 1; // Increment the operation count
 
-            for (int k = 0; k < n; ++k) {
-                if (k != i && k != j) {
-                    if (!arePointsOnSameSide(points[i], points[j], points[k], points[(k + 1) % n])) {
+        for (int j = i + 1; j < n; j++) { // j = i + 1 to avoid unnecessary iterations
+            numOperations += 1; // Increment the operation count
+
+            bool allPointsOnSameSide = true;
+            for (int k = 0; k < n; k++) {
+                numOperations += 1; // Increment the operation count
+
+                if (k != i && k != j && (((k + 1) % n) != i) && (((k + 1) % n) != j)) {
+                    numOperations += 1; // Increment the operation count
+
+                    if (!arePointsOnSameSide(points[i], points[j], points[k], points[(k + 1) % n], numOperations)) {
                         allPointsOnSameSide = false;
                         break;
                     }
@@ -63,11 +78,15 @@ std::vector<Point> findConvexHull(const std::vector<Point>& points) {
             }
 
             if (allPointsOnSameSide) {
-                // Check if the line segment is part of the convex hull
+                // Check if the line segment is part of the
                 bool isValidSegment = true;
-                for (int k = 0; k < n; ++k) {
+                for (int k = 0; k < n; k++) {
+                    numOperations += 1; // Increment the operation count
+
                     if (k != i && k != j) {
-                        if (areCollinear(points[i], points[j], points[k])) {
+                        numOperations += 1; // Increment the operation count
+
+                        if (areCollinear(points[i], points[j], points[k], numOperations)) {
                             isValidSegment = false;
                             break;
                         }
@@ -75,6 +94,8 @@ std::vector<Point> findConvexHull(const std::vector<Point>& points) {
                 }
 
                 if (isValidSegment) {
+                    numOperations += 4; // Increment the operation count
+
                     uniquePoints.insert(points[i]);
                     uniquePoints.insert(points[j]);
                 }
@@ -87,37 +108,40 @@ std::vector<Point> findConvexHull(const std::vector<Point>& points) {
 
     return convexHull;
 }
-
 int main() {
-    std::vector<Point> points1 = { {0, 0}, {1, 1}, {2, 2}, {3, 1}, {2, 0} };
-    std::vector<Point> points2 = { {0, 0}, {1, 1}, {2, 2}, {3, 3}, {4, 4} };
-    std::vector<Point> points3 = { {0, 0}, {1, 1}, {2, 2}, {3, 3}, {0, 3} };
-    std::vector<Point> points4 = { {0, 0}, {1, 2}, {3, 1}, {2, 3}, {4, 4}, {1, 4}, {3, 2}, {4, 1} };
+    std::vector<Point> points1 = { {0, 0}, {1, 1}, {2, 2},{3,0} };
+    std::vector<Point> points2 = { { {0, 0}, {1, 0}, {2, 0}, {3, 0}, {4, 0}}};
+    std::vector<Point> points3 = { {0, 0}, {1, 0}, {2, 0}, {3, 3}, {0, 3} };
+    std::vector<Point> points4 = { {0, 0}, {1, 0}, {2, 0}, {2, 1}, {2, 2}, {1, 2}, {0, 2}, {0, 1} };
 
-    std::vector<Point> convexHull1 = findConvexHull(points1);
-    std::vector<Point> convexHull2 = findConvexHull(points2);
-    std::vector<Point> convexHull3 = findConvexHull(points3);
-    std::vector<Point> convexHull4 = findConvexHull(points4);
+    int numOperations = 0; // Counter for the number of operations
+
+    std::vector<Point> convexHull1 = findConvexHull(points1, numOperations);
+
 
     std::cout << "Convex Hull Points 1:\n";
     for (const Point& p : convexHull1) {
         std::cout << "(" << p.x << ", " << p.y << ")\n";
     }
-
+    std::cout << "Total Number of Operations: " << numOperations << std::endl;
+    std::vector<Point> convexHull2 = findConvexHull(points2, numOperations);
     std::cout << "Convex Hull Points 2:\n";
     for (const Point& p : convexHull2) {
         std::cout << "(" << p.x << ", " << p.y << ")\n";
     }
-
+    std::cout << "Total Number of Operations: " << numOperations << std::endl;
+    std::vector<Point> convexHull3 = findConvexHull(points3, numOperations);
     std::cout << "Convex Hull Points 3:\n";
     for (const Point& p : convexHull3) {
         std::cout << "(" << p.x << ", " << p.y << ")\n";
     }
-
+    std::cout << "Total Number of Operations: " << numOperations << std::endl;
+    std::vector<Point> convexHull4 = findConvexHull(points4, numOperations);
     std::cout << "Convex Hull Points 4:\n";
     for (const Point& p : convexHull4) {
         std::cout << "(" << p.x << ", " << p.y << ")\n";
     }
+    std::cout << "Total Number of Operations: " << numOperations << std::endl;
 
     return 0;
 }
--- Divide And Conquer/Divide And Conquer/Divide And Conquer.cpp
+++ Divide And Conquer/Divide And Conquer/Divide And Conquer.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
 
 // Structure to represent a point in the Cartesian plane
 struct Point {
@@ -10,22 +11,33 @@ struct Point {
 // Returns 0 for collinear, 1 for clockwise, and 2 for counterclockwise
 int orientation(const Point& p, const Point& q, const Point& r) {
     int val = (q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y);
-    if (val == 0)
-        return 0;  // Collinear points
+    if (val == 0) {
+        return 0; // Collinear points
+    }
     return (val > 0) ? 1 : 2; // Clockwise or counterclockwise
+}
+
+// Function to calculate the squared distance between a point and a line
+int squaredDistanceToLine(const Point& p, const Point& lineStart, const Point& lineEnd) {
+    int dx = lineEnd.x - lineStart.x;
+    int dy = lineEnd.y - lineStart.y;
+    int numerator = std::abs(dy * p.x - dx * p.y + lineEnd.x * lineStart.y - lineEnd.y * lineStart.x);
+    int denominator = dx * dx + dy * dy;
+    return (numerator * numerator) / denominator;
 }
 
 // Recursive function to find the convex hull given a set of points
 void findHull(const std::vector<Point>& points, int n, const Point& p1, const Point& pn, std::vector<Point>& hull) {
-    if (n < 1)
+    if (n < 1) {
         return;
+    }
 
     int maxDist = -1;
     int maxIndex = -1;
 
     // Find the point with the maximum distance from the line (p1, pn)
     for (int i = 0; i < n; i++) {
-        int dist = std::abs(orientation(p1, pn, points[i]));
+        int dist = squaredDistanceToLine(points[i], p1, pn);
         if (dist > maxDist) {
             maxDist = dist;
             maxIndex = i;
@@ -39,10 +51,12 @@ void findHull(const std::vector<Point>& points, int n, const Point& p1, const Po
 
     // Partition the points into two sets based on their orientation
     for (int i = 0; i < n; i++) {
-        if (orientation(p1, pFarthest, points[i]) == 2)
+        if (orientation(p1, pFarthest, points[i]) == 2) {
             leftSet.push_back(points[i]);
-        else if (orientation(pFarthest, pn, points[i]) == 2)
+        }
+        else if (orientation(pFarthest, pn, points[i]) == 2) {
             rightSet.push_back(points[i]);
+        }
     }
 
     // Recursively find the convex hull of the left and right sets
@@ -53,36 +67,38 @@ void findHull(const std::vector<Point>& points, int n, const Point& p1, const Po
 // Function to compute the convex hull of a set of points
 std::vector<Point> computeConvexHull(std::vector<Point>& points) {
     int n = points.size();
-    if (n < 3)
-        return {}; // Not enough points to form a convex hull
-
+    if (n < 3) {
+        return {}; // Not enough points
+    }
     std::vector<Point> hull;
     int min_x = 0, max_x = 0;
 
     // Find the point with the minimum and maximum x-coordinates
     for (int i = 1; i < n; i++) {
-        if (points[i].x < points[min_x].x)
+        if (points[i].x < points[min_x].x) {
             min_x = i;
-        if (points[i].x > points[max_x].x)
+        }
+        if (points[i].x > points[max_x].x) {
             max_x = i;
+        }
     }
 
     const Point& p1 = points[min_x];
     const Point& pn = points[max_x];
-    hull.push_back(p1);
 
-    // If p1 and pn are distinct points, add pn to the hull
-    if (p1.x != pn.x || p1.y != pn.y)
-        hull.push_back(pn);
+    hull.push_back(p1);
+    hull.push_back(pn);
 
     std::vector<Point> leftSet, rightSet;
 
     // Partition the points into two sets based on their orientation
     for (int i = 0; i < n; i++) {
-        if (orientation(p1, pn, points[i]) == 2)
+        if (orientation(p1, pn, points[i]) == 2) {
             leftSet.push_back(points[i]);
-        else if (orientation(pn, p1, points[i]) == 2)
+        }
+        else if (orientation(pn, p1, points[i]) == 2) {
             rightSet.push_back(points[i]);
+        }
     }
 
     // Recursively find the convex hull of the left and right sets
@@ -91,7 +107,6 @@ std::vector<Point> computeConvexHull(std::vector<Point>& points) {
 
     return hull;
 }
-
 int main() {
     std::vector<Point> points1 = { {0, 3}, {1, 1}, {2, 2}, {4, 4}, {0, 0}, {1, 2}, {3, 1}, {3, 3} };
     std::vector<Point> convexHull1 = computeConvexHull(points1);
@@ -100,15 +115,17 @@ int main() {
     for (const auto& point : convexHull1) {
         std::cout << "(" << point.x << ", " << point.y << ")\n";
     }
+
     std::cout << "\n";
 
-    std::vector<Point> points2 = { {0, 0}, {1, 1}, {2, 2}, {3, 3}, {4, 4}, {1, 3}, {3, 1} };
+    std::vector<Point> points2 = { { {0, 0}, {1, 1}, {2, 2}, {3, 3}, {4, 4}, {1, 3}, {3, 1} } };
     std::vector<Point> convexHull2 = computeConvexHull(points2);
 
     std::cout << "Convex Hull Points 2:\n";
     for (const auto& point : convexHull2) {
         std::cout << "(" << point.x << ", " << point.y << ")\n";
     }
+
     std::cout << "\n";
 
     std::vector<Point> points3 = { {0, 0}, {0, 1}, {0, 2}, {0, 3}, {0, 4}, {1, 1}, {1, 3}, {3, 1}, {3, 3} };
@@ -118,6 +135,7 @@ int main() {
     for (const auto& point : convexHull3) {
         std::cout << "(" << point.x << ", " << point.y << ")\n";
     }
+
     std::cout << "\n";
 
     std::vector<Point> points4 = { {0, 0}, {1, 0}, {2, 0}, {3, 0}, {4, 0}, {1, -1}, {3, -1} };
@@ -127,6 +145,7 @@ int main() {
     for (const auto& point : convexHull4) {
         std::cout << "(" << point.x << ", " << point.y << ")\n";
     }
+
     std::cout << "\n";
 
     return 0;
